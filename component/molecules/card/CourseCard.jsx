13394024e2b2700bfd68307e285d2faf2af3d2df
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-05-06 15:22:28 +0530

    card

--- component/molecules/card/CourseCard.jsx
+++ component/molecules/card/CourseCard.jsx
@@ -1,16 +1,15 @@
-
 const CourseCard = () => {
   return (
     <div className="w-full px-4 py-4">
-      <div className="flex flex-col sm:flex-row gap-4 rounded-xl border border-gray-300  w-full max-w-md mx-auto">
-       <div className="w-64 h-44 " >
-       <img
-          className="h-44  md:w-72 w-72 sm:w-32 object-cover rounded-xl"
-          src="https://img.freepik.com/free-photo/education-students-happy-asian-woman-holding-notebooks-laughing-smiling-camera-enjoys-goi_1258-167794.jpg?semt=ais_hybrid&w=740"
-          alt="Course"
-        />
-       </div>
-        <div className="flex flex-col justify-between m-5 w-full">
+      <div className="flex flex-col sm:flex-row gap-4 rounded-xl border border-gray-300 w-full max-w-md mx-auto">
+        <div className="w-full sm:w-1/3">
+          <img
+            className="h-44 w-full object-cover rounded-t-xl sm:rounded-l-xl sm:rounded-tr-none"
+            src="https://img.freepik.com/free-photo/education-students-happy-asian-woman-holding-notebooks-laughing-smiling-camera-enjoys-goi_1258-167794.jpg?semt=ais_hybrid&w=740"
+            alt="Course"
+          />
+        </div>
+        <div className="flex flex-col justify-between p-4 w-full">
           <div>
             <div className="flex justify-between items-center">
               <span className="text-yellow-400 font-poppins">Digital Marketing</span>
@@ -18,9 +17,9 @@ const CourseCard = () => {
                 <i className="ri-arrow-up-circle-line text-xl"></i>
               </span>
             </div>
-            <h1 className="font-bold text-2xl  mt-1">Course Name</h1>
+            <h1 className="font-bold text-2xl mt-1">Course Name</h1>
             <p className="text-sm text-[#202224] mt-3">
-              <i className="ri-star-fill  text-yellow-400"></i> 4.2 | 4 Hrs 05 Mins
+              <i className="ri-star-fill text-yellow-400"></i> 4.2 | 4 Hrs 05 Mins
             </p>
           </div>
           <div className="mt-2">
@@ -34,4 +33,3 @@ const CourseCard = () => {
 };
 
 export default CourseCard;
-
